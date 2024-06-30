7cf6dc8222241c778e3518a2cf634e0e57844a75
debuged <debuged@outlook.com>
2024-06-30 18:15:06 +0800

    初始提交：创建Spring Boot应用的基本结构
    - 添加`.gitignore`文件，忽略IDE和构建工具的临时文件。
    - 创建`application.properties`文件，设置Spring Boot应用的名称。
    - 创建`pom.xml`文件，定义Maven项目配置，包括依赖和构建插件。- 添加`SpringbootApplication.java`文件，作为Spring Boot应用的入口点。

---
+++ src/main/java/cn/debuged/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package cn.debuged.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Debuged
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
